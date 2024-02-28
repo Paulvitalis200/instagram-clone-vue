63a6434c510d0f858ec13c48f28e99ce93641615
Paul Otieno <vitalispaul48@live.com>
2024-02-28 14:50:34 +0300

    Add timeline

---
+++ src/components/Timeline.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Container from './Container.vue';
+import Card from './Card.vue';
+const data = [
+    {
+        id: 1,
+        username: 'Pablo',
+        url: 'https://www.brides.com/thmb/sAyc2txVvmJXbZFoqZZPm3Vf5_Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/01selenagomez-f35b65bd620b43c4b038a19832967121.jpg',
+        caption: "I like pink"
+    },
+    {
+        id: 2,
+        username: 'The Weekend',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPK759DQ5AtAP_iov8YE1X6rPlEeHenfGQe3cr-YdyQ&s',
+        caption: "Hello guys"
+    }
+]
+</script>
+
+
+<template>
+    <Container>
+        <div class="timeline-container">
+            <Card v-for="post in data" :key="post.id" :post="post"/>
+        </div>
+       
+    </Container>
+</template>
+
+<style scoped>
+.timeline-container {
+   
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 0;
+}
+</style>
